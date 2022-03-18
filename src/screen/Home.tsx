59ec059d56fb95c5c89f2cba46dfd5224f46310f
fine <dlgmltjr0925@gmail.com>
2022-03-19 00:20:20 +0900

    React stack navigation 이동 로직 추가

--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -1,13 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import React, { useCallback } from 'react';
+import { StackActions, useNavigation } from '@react-navigation/native';
 
 export default function Home() {
+  const navigation = useNavigation();
+
+  const handlePress = useCallback(
+    (screenName: string) => {
+      return () => navigation.dispatch(StackActions.push(screenName));
+    },
+    [navigation],
+  );
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Pressable style={styles.buttonWrapper} onPress={handlePress('Profile')}>
+        <Text>Profile</Text>
+      </Pressable>
+      <Pressable style={styles.buttonWrapper} onPress={handlePress('Settings')}>
+        <Text>Settings</Text>
+      </Pressable>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  buttonWrapper: {
+    width: 200,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    marginTop: 10,
+  },
+});
