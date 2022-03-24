cca9ae0914bfc2bf1df5d2eb8c9d965389fb813f
fine <dlgmltjr0925@gmail.com>
2022-03-25 01:58:55 +0900

    useFocusContext 구현

--- src/screen/NestedStack.tsx
+++ src/screen/NestedStack.tsx
@@ -1,16 +1,39 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native';
-import React, { useCallback } from 'react';
-import { StackActions, useNavigation } from '@react-navigation/native';
+import React, { useCallback, useContext, useState } from 'react';
+import {
+  StackActions,
+  useFocusEffect,
+  useIsFocused,
+  useNavigation,
+} from '@react-navigation/native';
 import { decrement, increment } from '../feature/counter/counterSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Counter from '../component/Counter';
+import { CounterContext } from '../context/CounterContext';
 import { RootState } from '../store';
-import useIsFocused from '../hook/useIsFocused';
+
+// import useIsFocused from '../hook/useIsFocused';
 
 interface NestedStackProps {
   index: number;
 }
+
+const useFocusContext = (reactContext: React.Context<any>) => {
+  const context = useContext(reactContext);
+  const [contextValue, setContextValue] = useState(context);
+
+  useFocusEffect(() => {
+    console.log('useFocusEffect1');
+    if (contextValue !== context) {
+      console.log('useFocusEffect2');
+
+      setContextValue(context);
+    }
+  });
+
+  return contextValue;
+};
 
 export default function NestedStack({ index }: NestedStackProps) {
   const screenKey = `NestedStack ${index}`;
@@ -18,24 +41,25 @@ export default function NestedStack({ index }: NestedStackProps) {
 
   const navigation = useNavigation();
 
-  // const counter = useContext(CounterContext);
-  const isFocused = useIsFocused();
-  const count = useSelector(
-    (state: RootState) => state.counter.count,
-    (prev, next) => {
-      console.log(screenKey, isFocused, prev, next);
-      return !navigation.isFocused() || prev === next;
-    },
-  );
-  const dispatch = useDispatch();
+  const counter = useFocusContext(CounterContext);
 
-  const increase = useCallback(() => {
-    dispatch(increment());
-  }, [dispatch]);
+  // const isFocused = useIsFocused();
+  // const count = useSelector(
+  //   (state: RootState) => state.counter.count,
+  //   (prev, next) => {
+  //     console.log(screenKey, isFocused, prev, next);
+  //     return !navigation.isFocused() || prev === next;
+  //   },
+  // );
+  // const dispatch = useDispatch();
 
-  const decrease = useCallback(() => {
-    dispatch(decrement());
-  }, [dispatch]);
+  // const increase = useCallback(() => {
+  //   dispatch(increment());
+  // }, [dispatch]);
+
+  // const decrease = useCallback(() => {
+  //   dispatch(decrement());
+  // }, [dispatch]);
 
   const handlePress = useCallback(
     () => navigation.dispatch(StackActions.push(`NestedStack${index + 1}`)),
@@ -47,7 +71,8 @@ export default function NestedStack({ index }: NestedStackProps) {
       <Pressable style={styles.buttonWrapper} onPress={handlePress}>
         <Text>{`NestedStack ${index + 1}`}</Text>
       </Pressable>
-      <Counter count={count} increase={increase} decrease={decrease} />
+      {/* <Counter count={count} increase={increase} decrease={decrease} /> */}
+      <Counter {...counter} />
     </View>
   );
 }
