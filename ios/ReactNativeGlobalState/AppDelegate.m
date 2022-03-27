d62585f758884517cdf82b7f0ea5183ed03d51fb
fine <dlgmltjr0925@gmail.com>
2022-03-27 16:22:12 +0900

    rtl 환경을 위한 ar 언어 추가

--- ios/ReactNativeGlobalState/AppDelegate.m
+++ ios/ReactNativeGlobalState/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <React/RCTI18nUtil.h>
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -30,11 +31,14 @@ static void InitializeFlipper(UIApplication *application) {
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
-
+  
+  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"ReactNativeGlobalState"
                                             initialProperties:nil];
+  
+  [[RCTI18nUtil sharedInstance] allowRTL:YES];
 
   if (@available(iOS 13.0, *)) {
       rootView.backgroundColor = [UIColor systemBackgroundColor];
